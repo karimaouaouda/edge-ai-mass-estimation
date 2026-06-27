4d0c00615a3489ed5a6f9ae041ae026c84ad2911
karim aouaouda <karimaouaouda.officiel@gmail.com>
2026-06-27 19:55:44 +0100

    re add organic waste

--- src/edge_ai_mass/training/yolo.py
+++ src/edge_ai_mass/training/yolo.py
@@ -330,7 +330,7 @@ class YOLOTrainer:
                         )
                     ),
                     batch=int(batch/2),
-                    device="0,1",
+                    device="0",
                     conf=float(evaluation.get("conf", 0.001)),
                     iou=float(evaluation.get("iou", 0.7)),
                     plots=bool(evaluation.get("plots", True)),
